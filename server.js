92f577818c0457a09b68796057a16b7863f5a804
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-08 22:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(content[articleName]));
@@ -82,12 +88,6 @@ app.get('/:articleName', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter + 1;
-  res.send(counter.toString());
 });
 
 app.get('/ui/main.js', function (req, res) {
