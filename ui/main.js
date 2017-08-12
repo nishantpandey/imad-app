d6efda5052c07d7b47492c466dc066b69cd42809
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-12 14:36:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,6 +38,6 @@ submit.onclick = function() {
             }
          }
       };
-    request.open('GET', 'http://nishantbitsmesra.imad.hasura-app.io/submit-name?=' + name, true);
+    request.open('GET', 'http://nishantbitsmesra.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
