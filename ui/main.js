596725e781f56d77334f2a293fa62023d136022d
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-12 14:06:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,6 @@ submit.onclick = function() {
        list += '<ul>' + names[i] + '</ul>';
    }
    
-   var ul = document.getElementByID('name-list');
+   var ul = document.getElementById('name-list');
    ul.innerHTML = list;
 };
