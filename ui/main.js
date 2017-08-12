69fb18f2e7c4fa4ae0fc217c8b626d940309b975
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-12 14:31:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,12 +20,24 @@ button.onclick = function() {
 
 var submit = document.getElementById("submit-btn");
 submit.onclick = function() {
-   var names = ["name1", "name2", "name3"];
-   var list = '';
-   for(var i=0;i<names.length;i++) {
-       list += '<ul>' + names[i] + '</ul>';
-   }
-   
-   var ul = document.getElementById('namelist');
-   ul.innerHTML = list;
+    var name = document.getElementById("name");
+    name = name.value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++) {
+                  list += '<ul>' + names[i] + '</ul>';
+                }
+       
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+         }
+      };
+    request.open('GET', 'http://nishantbitsmesra.imad.hasura-app.io/submit-name?' + name, true);
+    request.send(null);
 };
