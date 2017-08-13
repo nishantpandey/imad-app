759a823136c9cd59fa50c4db135b8bf6d167ec22
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-13 18:21:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ var content = {
                 <input type="button" onclick="getwords()" value="Enter" />
               </form>
               <div class="footer">
-                <p id="para"></p>
+                <p id="para" border = "1"></p>
               <div>
             </div>
             <script type="text/javascript">
