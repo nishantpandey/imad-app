0113d32c4c3f96828d8c0c502710137c2538f375
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-19 16:55:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var pool = require('pg').Pool;
+
+var config = {
+    user :'nishantbitsmesra',
+    database : 'nishantbitsmesra',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var content = {
         'article-one' :{
@@ -94,6 +103,18 @@ var content = {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res) {
+   pool.query('SELECT * FROM test ', function(err,result)) {
+     if(err) {
+         res.status(500).send(err.toString());
+     }else {
+         res.send(JSON.stringify(result.row));
+     }
+   });
+    
 });
 
 
