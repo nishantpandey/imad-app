d769357b86f8fcbdb464879aacd74727f3e534a9
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-19 17:00:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan('combined'));
 var config = {
     user :'nishantbitsmesra',
     database : 'nishantbitsmesra',
-    host: 'http://db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
