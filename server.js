b44b5fca274023c36290ef75f439c4833a507f7a
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-22 17:31:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input, salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, S12, 'shas12');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
