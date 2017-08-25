4415ccbc1c6441e32110e7a47a9b861fc8c68260
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-25 17:16:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,6 @@
 console.log('Loaded!');
-var button = document.getElementById("counter");
 
-button.onclick = function() {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                 span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    
-   request.open('GET', 'http://nishantbitsmesra.imad.hasura-app.io/counter', true);
-   request.send(null);
-};
-
-var submit = document.getElementById("submit-btn");
+var submit = document.getElementById("submit_butn");
 submit.onclick = function() {
     var name = document.getElementById("name");
     name = name.value;
@@ -26,18 +8,18 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE) {
             if(request.status === 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for(var i=0;i<names.length;i++) {
-                  list += '<ul>' + names[i] + '</ul>';
-                }
-       
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;
+             alert('logged in successfully');   
+            }else if(request.status === 403) {
+                alert('log was un sucessfull');
+            }else if(request.status === 500) {
+                alert('Something went wrong at server side');
             }
-         }
-      };
-    request.open('GET', 'http://nishantbitsmesra.imad.hasura-app.io/submit-name?name=' + name, true);
-    request.send(null);
+        }
+    };
+     
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    
+    request.open('POST', 'http://nishantbitsmesra.imad.hasura-app.io/login', true);
+    request.send(JSON.Stringify({username: username, password: password}));
 };
