fee20143a7a21204411da2514d4efd3b6b83cf2d
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-26 17:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,6 @@ submit.onclick = function() {
     console.log(password);
     
     request.open('POST', 'http://nishantbitsmesra.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
