2f5706424ffbf9d52fd2ff08ddb65158b5997e7c
nishantpandey <nishant.bits.mesra@gmail.com>
2017-08-26 16:56:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,6 +149,24 @@ app.post('/create-user', function(req, res){
 app.post('/login', function(req,res) {
    var username = req.body.username;
    var password = req.body.password;
+   pool.query('SELECT * from "user" WHERE username = $1', [username], function(result, error) {
+       if(error) {
+           res.status(500).send(error.toString());
+       }else if(result.rows.length === 0) {
+           res.send(403).send("Username/Password is uncorrect");
+       }else {
+           var dbString = result.rows[0].password;
+           var salt = dbString.split('$')[2];
+           var hashedPassword = hash(password, salt);
+           if(hashedPassword === password) {
+               res.send('Credentials Correct');
+           }else {
+               res.send(403).send("Username/Password is uncorrect");
+           }
+           
+       }
+       
+   });
    
     
 });
